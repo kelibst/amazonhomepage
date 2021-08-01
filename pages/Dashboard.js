b1b6e569cef8e8de5964625e8b7fc5ce07508f7d
Keli Booster <kbooster17@gmail.com>
2021-08-01 13:08:45 +0000

    successfull redirect

---
+++ pages/Dashboard.js
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    const currentUser = localStorage.getItem("currentuser");
+    
+    return (
+        <div className="userpage">Hello World</div>
+    )
+}
+
+export default Dashboard
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,71 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import Link from 'next/link'
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/dist/client/router';
 
 export default function Home() {
+  const initialState = {
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  };
+  const router = useRouter()
+  const [userData, setUserData] = useState(initialState);
+  const [errors, setErrors] = useState({ error: false, errors: [] });
+  const [isSubmit, setisSubmit] = useState(false)
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
+  const setUser = (userData) => {
+    let usersData = localStorage.getItem("user")
+    if (usersData?.length) {
+      usersData = JSON.parse(usersData)
+      usersData.push(userData)
+      debugger
+    } else {
+      usersData = []
+      usersData.push(userData)
+      debugger
+    }
+
+  }
+
   const registerUser = (e) => {
     e.preventDefault();
+    const { password, password_confirmation } = userData
+    setisSubmit(true)
+
+    if (password === password_confirmation) {
+      setUser(userData)
+
+      router.push('/Dashboard')
+    } else {
+      setErrors({
+        ...errors,
+        error: true,
+        errors: [
+          ...errors,
+          "Password and Password confirmation are not the same."
+        ]
+      })
+    }
+
   }
+
+  // useEffect(() => {
+
+  //   setUser(userData)
+  // }, [isSubmit, userData, errors])
   return (
     <div className={styles.container}>
       <Head>
@@ -21,27 +81,30 @@ export default function Home() {
             <div className="logo-container">
               <img src="/assets/amazon-icon.jpg" className="logo-img" alt="amazon logo" />
             </div>
+            {errors?.error && <div className="errors-container"> {
+              errors?.errors?.map(err => <li>{err}</li>)
+            }</div>}
             <div className={styles.loginform}>
               <h1 className={styles.formh1}>Create account</h1>
               <form onSubmit={registerUser}>
                 <div className={styles.formcontrol}>
                   <label htmlFor="name">Your name</label>
-                  <input id="name" type="text" autoComplete="name" required />
+                  <input name="name" type="text" autoComplete="name" onChange={handleChange} required />
                 </div>
                 <div className={styles.formcontrol}>
                   <label htmlFor="email">Email</label>
-                  <input id="email" type="email" autoComplete="email" required />
+                  <input name="email" type="email" autoComplete="email" onChange={handleChange} required />
                 </div>
 
                 <div className={styles.formcontrol}>
                   <label htmlFor="password">Password</label>
-                  <input id="password" type="password" required />
-                  <p className="password-info"><Image src="/assets/info-icon.png" alt="info icon" className="more info" height="15" width="10" /> Passwords must be at least 6 characters.</p>
+                  <input name="password" placeholder="At least 6 characters" onChange={handleChange} type="password" required />
+                  <div className="password-info"><Image src="/assets/info-icon.png" alt="info icon" className="more info" height="15" width="10" /> Passwords must be at least 6 characters.</div>
                 </div>
 
                 <div className={styles.formcontrol}>
                   <label htmlFor="password_confirmation">Password</label>
-                  <input id="password_confirmation" type="password" required />
+                  <input name="password_confirmation" type="password" onChange={handleChange} required />
                 </div>
                 <button className={styles.loginbtn} type="submit">Create your Amazon account</button>
 
@@ -57,13 +120,13 @@ export default function Home() {
         </div>
       </main>
       <footer className="loginfooter">
-      <div className={styles.logindivider}></div>
-      <ul className="footer-ul">
-        <li><Link href="#"><a>Conditions of use</a></Link></li>
-        <li><Link href="#"><a>Privacy Notice</a></Link></li>
-        <li><Link href="#"><a>Help</a></Link></li>
-      </ul>
-      <p className="copy">© 1996-2021, Amazon.com, Inc. or its affiliates</p>
+        <div className={styles.logindivider}></div>
+        <ul className="footer-ul">
+          <li><Link href="#"><a>Conditions of use</a></Link></li>
+          <li><Link href="#"><a>Privacy Notice</a></Link></li>
+          <li><Link href="#"><a>Help</a></Link></li>
+        </ul>
+        <p className="copy">© 1996-2021, Amazon.com, Inc. or its affiliates</p>
       </footer>
     </div>
   )
